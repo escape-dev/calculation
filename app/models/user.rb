00e8fd6ed44846fd5ef72753708b095b78e39838
hafiz <hafiz170301@gmail.com>
2024-05-14 23:38:45 +0700

    feat: create register page and adjust login page styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :email, :password, presence: :true
 end
