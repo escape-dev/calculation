952b2217a84932bcde4c7492f9790e88ee7c6f75
hafiz <hafiz170301@gmail.com>
2024-08-15 16:37:22 +0700

    feat: generate model and migration for categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
+  has_many :categories
+
   validates :email, :password, presence: :true
+
 end
